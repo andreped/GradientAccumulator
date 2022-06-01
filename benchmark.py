180cabc3f1b603bdc00102a6f466669f98770a28
andreped <andrped94@gmail.com>
2022-06-01 21:51:38 +0200

    created GAModelWrapper method - benchmarked it and it appears to work well!!

--- benchmark.py
+++ benchmark.py
@@ -29,6 +29,7 @@ import tensorflow_datasets as tfds
 from tensorflow.keras.models import load_model
 from GradientAccumulator.accumulator import GradientAccumulator, OldGradientAccumulator
 from GradientAccumulator.adamAccumulate import AdamAccumulated
+from GradientAccumulator.GAModelWrapper import GAModelWrapper
 import argparse
 import sys
 
@@ -36,48 +37,6 @@ import sys
 def normalize_img(image, label):
     """Normalizes images: `uint8` -> `float32`."""
     return tf.cast(image, tf.float32) / 255., label
-
-
-# https://stackoverflow.com/a/66524901
-class CustomModel(tf.keras.Model):
-    def __init__(self, n_gradients, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.n_gradients = tf.constant(n_gradients, dtype=tf.int32)
-        self.n_acum_step = tf.Variable(0, dtype=tf.int32, trainable=False)
-        self.gradient_accumulation = [tf.Variable(tf.zeros_like(v, dtype=tf.float32), trainable=False) for v in self.trainable_variables]
-
-    def train_step(self, data):
-        self.n_acum_step.assign_add(1)
-
-        x, y = data
-
-        # Gradient Tape
-        with tf.GradientTape() as tape:
-            y_pred = self(x, training=True)
-            loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
-
-        # Calculate batch gradients
-        gradients = tape.gradient(loss, self.trainable_variables)
-
-        # Accumulate batch gradients
-        for i in range(len(self.gradient_accumulation)):
-            self.gradient_accumulation[i].assign_add(gradients[i])
- 
-        # If n_acum_step reach the n_gradients then we apply accumulated gradients to update the variables otherwise do nothing
-        tf.cond(tf.equal(self.n_acum_step, self.n_gradients), self.apply_accu_gradients, lambda: None)
-
-        # update metrics
-        self.compiled_metrics.update_state(y, y_pred)
-        return {m.name: m.result() for m in self.metrics}
-
-    def apply_accu_gradients(self):
-        # apply accumulated gradients
-        self.optimizer.apply_gradients(zip(self.gradient_accumulation, self.trainable_variables))
-
-        # reset
-        self.n_acum_step.assign(0)
-        for i in range(len(self.gradient_accumulation)):
-            self.gradient_accumulation[i].assign(tf.zeros_like(self.trainable_variables[i], dtype=tf.float32))
 
 
 if __name__ == "__main__":
@@ -136,7 +95,7 @@ if __name__ == "__main__":
     elif ret.accum_opt == 3:
         opt = OldGradientAccumulator(opt, accum_steps=ret.accum_steps)
     elif ret.accum_opt == -1:
-        model = CustomModel(n_gradients=ret.accum_steps, inputs=model.input, outputs=model.output)
+        model = GAModelWrapper(n_gradients=ret.accum_steps, inputs=model.input, outputs=model.output)
     else:
         raise ValueError("Uknown accumulate gradient method was chosen. Available wrappers are: {0, 1, 2, 3}.")
 
