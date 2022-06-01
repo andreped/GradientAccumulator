479ecab98f7c93834e8f327c9d59bbab074f24fe
andreped <andrped94@gmail.com>
2022-06-01 16:38:10 +0200

    back to MEAN for testing

--- GradientAccumulator/accumulator.py
+++ GradientAccumulator/accumulator.py
@@ -12,7 +12,7 @@ class GradientAccumulator(tf.keras.optimizers.Optimizer):
         self,
         inner_optimizer: types.Optimizer,
         accum_steps: types.TensorLike = 4,
-        reduction: str = "SUM",
+        reduction: str = "MEAN",
         name: str = "GradientAccumulator",
         **kwargs,
     ):
