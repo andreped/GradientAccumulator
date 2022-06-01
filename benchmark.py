756038cc49968d8790b0f72f0596ff340f8fde3b
andreped <andrped94@gmail.com>
2022-06-01 20:41:04 +0200

    added argparse to run experiments without changing code

--- benchmark.py
+++ benchmark.py
@@ -27,6 +27,8 @@ import tensorflow_datasets as tfds
 from tensorflow.keras.models import load_model
 from GradientAccumulator.accumulator import GradientAccumulator, OldGradientAccumulator
 from GradientAccumulator.adamAccumulate import AdamAccumulated
+import argparse
+import sys
 
 
 def normalize_img(image, label):
@@ -78,11 +80,23 @@ class CustomModel(tf.keras.Model):
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batchsize', metavar='--bs', type=int, nargs='?', default=64,
+                        help="which batch size to use.")
+    parser.add_argument('--accum_steps', metavar='--acs', type=int, nargs='?', default=1,
+                        help="number of accumulation steps.")
+    parser.add_argument('--epochs', metavar='--eps', type=int, nargs='?', default=3,
+                        help="number of epochs.")
+    parser.add_argument('--accum_opt', metavar='--opt', type=int, nargs='?', default=2,
+                        help="which gradient accumulator approach to use. Four available: {0, 1, 2, 3, -1}.")
+    ret = parser.parse_args(sys.argv[1:]); print(ret)
+
+
     # params
-    batchsize = 64  # 8 vs 32
-    accum_steps = 1  # 4 vs 1
-    nb_epochs = 3  # 3
-    accum_opt = 2  # which implementation of accumulated gradients to use. Default 2 (should be the best one)
+    #batchsize = 64  # 8 vs 32
+    #accum_steps = 1  # 4 vs 1
+    #nb_epochs = 3  # 3
+    #accum_opt = 2  # which implementation of accumulated gradients to use. Default 2 (should be the best one)
 
     # load dataset
     (ds_train, ds_test), ds_info = tfds.load(
@@ -98,13 +112,13 @@ if __name__ == "__main__":
         normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
     ds_train = ds_train.cache()
     ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
-    ds_train = ds_train.batch(batchsize)
+    ds_train = ds_train.batch(ret.batchsize)
     ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
 
     # build test pipeline
     ds_test = ds_test.map(
         normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
-    ds_test = ds_test.batch(batchsize)
+    ds_test = ds_test.batch(ret.batchsize)
     ds_test = ds_test.cache()
     ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
 
@@ -118,16 +132,16 @@ if __name__ == "__main__":
     # compile model
     opt = tf.keras.optimizers.Adam(1e-3)
     # opt = tf.keras.optimizers.SGD(0.1)
-    if accum_opt == 0:
+    if ret.accum_opt == 0:
         pass  # no accumulated gradients - normal Adam
-    elif accum_opt == 1:
-        opt = AdamAccumulated(accumulation_steps=accum_steps)
-    elif accum_opt == 2:
-        opt = GradientAccumulator(opt, accum_steps=accum_steps)
-    elif accum_opt == 3:
-        opt = OldGradientAccumulator(opt, accum_steps=accum_steps)
-    elif accum_opt == -1:
-        model = CustomModel(n_gradients=accum_steps, inputs=model.input, outputs=model.output)
+    elif ret.accum_opt == 1:
+        opt = AdamAccumulated(accumulation_steps=ret.accum_steps)
+    elif ret.accum_opt == 2:
+        opt = GradientAccumulator(opt, accum_steps=ret.accum_steps)
+    elif ret.accum_opt == 3:
+        opt = OldGradientAccumulator(opt, accum_steps=ret.accum_steps)
+    elif ret.accum_opt == -1:
+        model = CustomModel(n_gradients=ret.accum_steps, inputs=model.input, outputs=model.output)
     else:
         raise ValueError("Uknown accumulate gradient method was chosen. Available wrappers are: {0, 1, 2, 3}.")
 
@@ -140,7 +154,7 @@ if __name__ == "__main__":
     # train model
     model.fit(
         ds_train,
-        epochs=nb_epochs * accum_steps,
+        epochs=ret.epochs * ret.accum_steps,
         validation_data=ds_test,
     )
 
