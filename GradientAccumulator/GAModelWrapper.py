06b9aaf5e90d6702c265ece1ab54f339dada02c2
andreped <andrped94@gmail.com>
2022-06-02 05:16:32 +0200

    support for sample_weight with GAModelWrapper

--- GradientAccumulator/GAModelWrapper.py
+++ GradientAccumulator/GAModelWrapper.py
@@ -2,22 +2,38 @@ import tensorflow as tf
 
 
 # https://stackoverflow.com/a/66524901
+# https://keras.io/guides/customizing_what_happens_in_fit/
 class GAModelWrapper(tf.keras.Model):
     def __init__(self, n_gradients, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.n_gradients = tf.constant(n_gradients, dtype=tf.int32)
         self.n_acum_step = tf.Variable(0, dtype=tf.int32, trainable=False)
-        self.gradient_accumulation = [tf.Variable(tf.zeros_like(v, dtype=tf.float32), trainable=False) for v in self.trainable_variables]
+        self.gradient_accumulation = [tf.Variable(tf.zeros_like(v, dtype=tf.float32), trainable=False) for v in
+                                      self.trainable_variables]
 
     def train_step(self, data):
         self.n_acum_step.assign_add(1)
 
-        x, y = data
+        # Unpack the data. Its structure depends on your model and
+        # on what you pass to `fit()`.
+        if len(data) == 3:
+            x, y, sample_weight = data
+        else:
+            sample_weight = None
+            x, y = data
 
         # Gradient Tape
         with tf.GradientTape() as tape:
-            y_pred = self(x, training=True)
-            loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+            y_pred = self(x, training=True)  # forward pass
+
+            # Compute the loss value.
+            # The loss function is configured in `compile()`.
+            loss = self.compiled_loss(
+                y,
+                y_pred,
+                sample_weight=sample_weight,
+                regularization_losses=self.losses,
+            )
 
         # Calculate batch gradients
         gradients = tape.gradient(loss, self.trainable_variables)
@@ -25,12 +41,12 @@ class GAModelWrapper(tf.keras.Model):
         # Accumulate batch gradients
         for i in range(len(self.gradient_accumulation)):
             self.gradient_accumulation[i].assign_add(gradients[i])
- 
+
         # If n_acum_step reach the n_gradients then we apply accumulated gradients to update the variables otherwise do nothing
         tf.cond(tf.equal(self.n_acum_step, self.n_gradients), self.apply_accu_gradients, lambda: None)
 
         # update metrics
-        self.compiled_metrics.update_state(y, y_pred)
+        self.compiled_metrics.update_state(y, y_pred, sample_weight=sample_weight)
         return {m.name: m.result() for m in self.metrics}
 
     def apply_accu_gradients(self):
