0910279e222d562b166275fb28ba734f3f927342
andreped <andrped94@gmail.com>
2022-06-04 15:03:01 +0200

    test Python 3.6

--- setup.py
+++ setup.py
@@ -18,11 +18,12 @@ setuptools.setup(
         'tensorflow-addons'
     ],
     classifiers=[
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 )
