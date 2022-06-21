fcb21aa5119b1ba9f08390998d153fd0fc336da4
dbouget <dbouget@users.noreply.github.com>
2022-06-21 15:01:41 +0200

    Update agc.py
    Attempt to fix #16

--- gradient_accumulator/agc.py
+++ gradient_accumulator/agc.py
@@ -27,8 +27,11 @@ def unitwise_norm(x):
     elif len(x.get_shape()) == 4:  # Conv kernels of shape HWIO
         axis = [0, 1, 2,]
         keepdims = True
+    elif len(x.get_shape()) == 5:  # Conv kernels of shape HWDIO
+        axis = [0, 1, 2, 3,]
+        keepdims = True
     else:
-        raise ValueError(f"Got a parameter with shape not in [1, 2, 4]! {x}")
+        raise ValueError(f"Got a parameter with shape not in [1, 2, 4, 5]! {x}")
     return compute_norm(x, axis, keepdims)
 
 
