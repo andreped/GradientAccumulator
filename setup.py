6e64586145f391f3af8fd827e6ffe6ff181c02b8
andreped <andrped94@gmail.com>
2022-08-25 19:13:02 +0200

    bumped version to 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="gradient-accumulator",
-    version="0.2.0",
+    version="0.2.1",
     author="André Pedersen and David Bouget",
     author_email="andrped94@gmail.com",
     description="Package for gradient accumulation in TensorFlow",
