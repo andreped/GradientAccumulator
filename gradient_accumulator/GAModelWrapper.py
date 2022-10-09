75c09b2374f0734cb2b9820998f85a7d2df211c8
andreped <andrped94@gmail.com>
2022-10-09 13:43:20 +0200

    added experimental support for multi-GPU strategy

--- gradient_accumulator/GAModelWrapper.py
+++ gradient_accumulator/GAModelWrapper.py
@@ -9,15 +9,20 @@ class GAModelWrapper(tf.keras.Model):
     def __init__(self, accum_steps=1, mixed_precision=False, use_agc=False, clip_factor=0.01, eps=1e-3, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.accum_steps = tf.constant(accum_steps, dtype=tf.int32, name="accum_steps")
-        self.accum_step_counter = tf.Variable(0, dtype=tf.int32, trainable=False, name="accum_counter")
-        self.gradient_accumulation = [tf.Variable(tf.zeros_like(v, dtype=tf.float32), trainable=False, name="accum_" + str(i)) for i, v in
-                                      enumerate(self.trainable_variables)]
+        self.accum_step_counter = tf.Variable(0, dtype=tf.int32, trainable=False, name="accum_counter",
+                                              synchronization=tf.VariableSynchronization.ON_READ,
+                                              aggregation=tf.VariableAggregation.ONLY_FIRST_REPLICA,
+                                              )
+        self.gradient_accumulation = [tf.Variable(tf.zeros_like(v, dtype=tf.float32), trainable=False,
+                                                  name="accum_" + str(i),
+                                                  synchronization=tf.VariableSynchronization.ON_READ,
+                                                  aggregation=tf.VariableAggregation.ONLY_FIRST_REPLICA,
+                                                  ) for i, v in enumerate(self.trainable_variables)]
         self.mixed_precision = mixed_precision
         self.use_agc = use_agc
         self.clip_factor = clip_factor
         self.eps = eps
 
-    # @tf.function  # https://www.tensorflow.org/guide/keras/writing_a_training_loop_from_scratch#speeding-up_your_training_step_with_tffunction
     def train_step(self, data):
         self.accum_step_counter.assign_add(1)
 
@@ -43,7 +48,7 @@ class GAModelWrapper(tf.keras.Model):
                 sample_weight=sample_weight,
                 regularization_losses=self.losses,
             )
-            loss = loss / tf.cast(self.accum_steps, tf.float32) # MEAN reduction here IMPORTANT! Don't do SUM reduction!!
+            loss = loss / tf.cast(self.accum_steps, tf.float32)  # MEAN reduction here IMPORTANT! Don't use SUM!
 
             # scale loss if mixed precision is enabled
             if self.mixed_precision:
@@ -58,14 +63,17 @@ class GAModelWrapper(tf.keras.Model):
 
         # apply adaptive gradient clipping -> should be AFTER unscaling gradients
         if self.use_agc:
-            gradients = agc.adaptive_clip_grad(self.trainable_variables, gradients, clip_factor=self.clip_factor, eps=self.eps)
+            gradients = agc.adaptive_clip_grad(
+                self.trainable_variables, gradients, clip_factor=self.clip_factor, eps=self.eps)
 
         # Accumulate batch gradients
         for i in range(len(self.gradient_accumulation)):
-            self.gradient_accumulation[i].assign_add(gradients[i])
+            self.gradient_accumulation[i].assign_add(gradients[i], read_value=False)
 
-        # If n_acum_step reach the accum_steps then we apply accumulated gradients to update the variables otherwise do nothing
-        tf.cond(tf.equal(self.accum_step_counter, self.accum_steps), true_fn=self.apply_accu_gradients, false_fn=lambda: None)
+        # If accum_step_counter reach the accum_steps then we apply accumulated gradients to update the variables
+        # otherwise do nothing
+        tf.cond(tf.equal(self.accum_step_counter, self.accum_steps), true_fn=self.apply_accu_gradients,
+                false_fn=lambda: None)
 
         # update metrics
         self.compiled_metrics.update_state(y, y_pred, sample_weight=sample_weight)
@@ -78,4 +86,5 @@ class GAModelWrapper(tf.keras.Model):
         # reset
         self.accum_step_counter.assign(0)
         for i in range(len(self.gradient_accumulation)):
-            self.gradient_accumulation[i].assign(tf.zeros_like(self.trainable_variables[i], dtype=tf.float32))
+            self.gradient_accumulation[i].assign(
+                tf.zeros_like(self.trainable_variables[i], dtype=tf.float32), read_value=False)
