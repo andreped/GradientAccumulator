a4389a4b428b7078fecfe7f33132576b7f4fd582
André Pedersen <andrped94@gmail.com>
2023-01-29 17:57:05 +0100

    bump v0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="gradient-accumulator",
-    version="0.2.2",
+    version="0.3.0",
     author="André Pedersen and David Bouget",
     author_email="andrped94@gmail.com",
     description="Package for gradient accumulation in TensorFlow",
