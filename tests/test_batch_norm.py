9e1e8d7e91bfed0ea031d849bff909d9c4d07334
André Pedersen <andrped94@gmail.com>
2023-04-06 21:11:49 +0200

    Added custom BN layer benchmark unit test

--- tests/test_batch_norm.py
+++ tests/test_batch_norm.py
@@ -10,7 +10,31 @@ def normalize_img(image, label):
     return tf.cast(image, tf.float32) / 255., label
 
 
-def test_batch_norm():
+def reset():
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+    # The below is necessary for starting Numpy generated random numbers
+    # in a well-defined initial state.
+    np.random.seed(123)
+
+    # The below is necessary for starting core Python generated random numbers
+    # in a well-defined state.
+    python_random.seed(123)
+
+    # The below set_seed() will make random number generation
+    # in the TensorFlow backend have a well-defined initial state.
+    # For further details, see:
+    # https://www.tensorflow.org/api_docs/python/tf/random/set_seed
+    tf.random.set_seed(1234)
+
+    # https://stackoverflow.com/a/71311207
+    tf.config.experimental.enable_op_determinism()
+
+    # disable GPU
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
+def run_experiment(custom_bn=True, accum=True):
     # load dataset
     (ds_train, ds_test), ds_info = tfds.load(
         'mnist',
@@ -37,13 +61,14 @@ def test_batch_norm():
     model = tf.keras.models.Sequential([
         tf.keras.layers.Flatten(input_shape=(28, 28)),
         tf.keras.layers.Dense(128),
-        AccumBatchNormalization(),
+        AccumBatchNormalization() if custom_bn else tf.keras.layers.BatchNormalization(),
         tf.keras.layers.Activation("relu"),
         tf.keras.layers.Dense(10)
     ])
 
     # wrap model to use gradient accumulation
-    model = GradientAccumulateModel(accum_steps=4, inputs=model.input, outputs=model.output)
+    if accum:
+        model = GradientAccumulateModel(accum_steps=4, inputs=model.input, outputs=model.output)
 
     # compile model
     model.compile(
@@ -66,8 +91,23 @@ def test_batch_norm():
     trained_model = load_model("./trained_model", compile=True)
 
     result = trained_model.evaluate(ds_test, verbose=1)
-    print(result)
+    return result
+
+
+def test_compare_bn_layers():
+    # custom BN without accum
+    result1 = run_experiment(custom_bn=True, accum=False)
+
+    # keras BN without accum
+    result2 = run_experiment(custom_bn=False, accum=False)
+
+    assert result1 == result2
+
+
+def test_custom_bn_accum_compatibility():
+    run_experiment(custom_bn=True, accum=True)
 
 
 if __name__ == "__main__":
-    test_batch_norm()
+    test_compare_bn_layers()
+    test_custom_bn_accum_compatibility()
