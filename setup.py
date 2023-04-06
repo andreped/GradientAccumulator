29f7af57a3e4d3be8eb6fce3cb315a1559cbd6f5
André Pedersen <andrped94@gmail.com>
2023-04-06 20:46:16 +0200

    Ignore notebooks in Package build

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/andreped/GradientAccumulator",
-    packages=setuptools.find_packages(exclude=('tests')),
+    packages=setuptools.find_packages(exclude=('tests', 'notebooks')),
     install_requires=[
         "tensorflow"
     ],
