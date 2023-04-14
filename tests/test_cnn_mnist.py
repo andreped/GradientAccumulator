3fa94ef9c5ae3a4f0b9e2c0835ae614edf339139
André Pedersen <andrped94@gmail.com>
2023-04-14 08:31:38 +0200

    Added cnn test to codecov CI

---
+++ tests/test_cnn_mnist.py
@@ -0,0 +1,73 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from tensorflow.keras.models import load_model
+from gradient_accumulator import GradientAccumulateModel
+
+
+def normalize_img(image, label):
+    """Normalizes images: `uint8` -> `float32`."""
+    return tf.cast(image, tf.float32) / 255., label
+
+
+def test_train_mnist():
+    # load dataset
+    (ds_train, ds_test), ds_info = tfds.load(
+        'mnist',
+        split=['train', 'test'],
+        shuffle_files=True,
+        as_supervised=True,
+        with_info=True,
+    )
+
+    # build train pipeline
+    ds_train = ds_train.map(normalize_img)
+    ds_train = ds_train.cache()
+    ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+    ds_train = ds_train.batch(128)
+    ds_train = ds_train.prefetch(1)
+
+    # build test pipeline
+    ds_test = ds_test.map(normalize_img)
+    ds_test = ds_test.batch(128)
+    ds_test = ds_test.cache()
+    ds_test = ds_test.prefetch(1)
+
+    model = tf.keras.models.Sequential([
+        tf.keras.Input(shape=input_shape),
+        tf.keras.layers.Conv2D(4, kernel_size=(3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(16, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(10, activation="softmax")
+    )]
+
+    # wrap model to use gradient accumulation
+    model = GradientAccumulateModel(accum_steps=4, inputs=model.input, outputs=model.output)
+
+    # compile model
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(1e-3),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+    )
+
+    # train model
+    model.fit(
+        ds_train,
+        epochs=1,
+        validation_data=ds_test,
+    )
+
+    model.save("./trained_model")
+
+    # load trained model and test
+    del model
+    trained_model = load_model("./trained_model", compile=True)
+
+    result = trained_model.evaluate(ds_test, verbose=1)
+    print(result)
+
+
+if __name__ == "__main__":
+    test_train_mnist()
