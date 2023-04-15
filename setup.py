b26c8d551b49a3cb80fa8ba985a07ca9a025a5d5
André Pedersen <andrped94@gmail.com>
2023-04-15 12:29:48 +0200

    Ignore docs + assets in build

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/andreped/GradientAccumulator",
-    packages=setuptools.find_packages(exclude=('tests', 'notebooks')),
+    packages=setuptools.find_packages(exclude=('tests', 'notebooks', 'assets', 'docs')),
     install_requires=[
         "tensorflow",
     ],
