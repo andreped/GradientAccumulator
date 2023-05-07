f4fff801c3092e378459959d0ef49827c3fd7a80
andreped <andrped94@gmail.com>
2023-05-07 21:29:22 +0200

    Added missing unitwise_norm import in __init__

--- gradient_accumulator/__init__.py
+++ gradient_accumulator/__init__.py
@@ -1,4 +1,4 @@
 from .accumulators import GradientAccumulateModel
 from .accumulators import GradientAccumulateOptimizer
-from .agc import adaptive_clip_grad
+from .agc import adaptive_clip_grad, unitwise_norm
 from .layers import AccumBatchNormalization
